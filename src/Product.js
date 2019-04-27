8a4891333a6b5561befedff3664ec231ccfa8fe0
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-04-27 14:55:10 -0400

    test1

--- src/Product.js
+++ src/Product.js
@@ -3,17 +3,26 @@ import React, {Component} from 'react';
 class Product extends Component{
     constructor(){
         super()
-        this.state = {}
+        this.state = {
+            name: ''
+        }
+    }
+
+    onChange = (ev) =>{
+        this.setState({[ev.target.name]: ev.target.value})
     }
 
     render(){
+        // fake data
+        const {name, value} = {name: 'sports', value: '50'}
+        const {onChange} = this
         return(
             <div>
                 <h1>
                     Showing individual Product
                 </h1>
                 <form>
-                    
+                    <input name={name} value={value} onChange={onChange} />
                 </form>
             </div>
         )
