f189ab14fb204aada2784f44ebf1d144630c2c2c
Ami Domadia <ami@Amis-MacBook-Pro.local>
2019-04-28 13:49:28 -0400

    adding product express router

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const db = require('../../db/models');
 const Product = db.Product;
-const Category = db.Category;
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     Product.findAll()
     .then((products) => res.json(products))
-    .catch(next)
+    .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+    if(req.params.id){
+        Product.findOne({where: {id: req.params.id}})
+        .then((product) => res.json(product))
+        .catch(next);
+    }
+});
+
+router.delete('/:id', (req, res, next) => {
+    if(req.params.id){
+        Product.destroy({where: {id: req.params.id}})
+        .then(() => res.send(204))
+        .catch(next);
+    }
 });
 
 module.exports = router;
