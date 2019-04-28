99d43f07e478d19496a5d5857f5d5bb45a64e634
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-04-28 11:01:59 -0400

    adding a cart image

--- src/Product.js
+++ src/Product.js
@@ -55,6 +55,8 @@ class Product extends Component{
                     </select>
                     <button className='btn btn-primary' type='submit'>Add to Cart</button>
                 </form>
+                <img className = 'shopping-cart' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKorRm0enmL_tFIgvKcNcOjb_3YkWnny-CIK0BW5F9DoGocc7DkA'/>
+                <span className = 'shopping-item-quantity'>{this.state.selectedQuantity}</span>
             </div>
         )
         
