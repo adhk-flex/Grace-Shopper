70f37729bdcb2a5237a756450cdf0c666d712574
Kristy Zhang <kristyso17@gmail.com>
2019-04-29 20:46:04 -0400

    store added lineitems thunk

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import axios from 'axios';
 
@@ -10,29 +10,68 @@ const setProducts = products => ({
   products
 });
 
-export const fetchProducts = () => dispatch => {
-  return axios.get('api/products/')
-    .then(products => dispatch(setProducts(products.data)))
-}
-
-const addProduct = product => dispatch => {
-  return axios.post('/api/products/', product)
-    .then(() => dispatch(fetchProducts()))
-}
-
-const delProduct = id => dispatch => {
-  return axios.delete(`/api/product/${id}`)
-    .then(() => dispatch(fetchProducts()))
-}
-
-const reducer = (state = [], action) => {
+const products = (state = [], action) => {
   switch (action.type) {
     case SET_PRODUCTS:
       return action.products;
     default:
+      return state
+  }
+};
+
+const SET_LINEITEMS = "SET_LINEITEMS";
+
+const setLineItems = items => ({
+  type: SET_LINEITEMS,
+  items
+});
+
+const lineItem = (state = [], action) => {
+  switch (action.type) {
+    case SET_LINEITEMS:
+      return action.items;
+    default:
       return state;
   }
 };
+
+export const fetchProducts = () => dispatch => {
+  return axios.get('api/products/')
+    .then(products => dispatch(setProducts(products.data)))
+};
+
+
+const addProduct = product => dispatch => {
+  return axios.post('/api/products/', product)
+    .then(() => dispatch(fetchProducts()))
+};
+
+const delProduct = id => dispatch => {
+  return axios.delete(`/api/product/${id}`)
+    .then(() => dispatch(fetchProducts()))
+};
+
+
+const fetchLineItems = () => dispatch => {
+  return axios.get('lineItem route')
+    .then(items => dispatch(setLineItems(items)))
+};
+
+
+const addLineItem = product => dispatch => {
+  return axios.post('lineItem route', product)
+    .then(() => dispatch(fetchLineItems()))
+};
+
+const delLineItem = id => dispatch => {
+  return axios.delete('lineItem route', id)
+    .then(() => dispatch(fetchLineItems()))
+};
+
+const reducer = combineReducers({
+  products,
+  lineItem
+});
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
