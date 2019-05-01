8f2578e8e1007ee5577799e4983605bae0ffbea3
Dave <dave.e.siegel@gmail.com>
2019-05-01 13:45:01 -0400

    Added hook to User to create new cart upon User creation. Started work on Order model

---
+++ db/models/Order.js
@@ -0,0 +1,36 @@
+const db = require("../db");
+const { Sequelize } = db;
+
+const Order = db.define("order", {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+    },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                args: true,
+                msg: "Order must have a status"
+            },
+            isIn: {
+                args: [["purchased", "shipped"]],
+                msg: "Order status must be either 'purchased' or 'shipped'"
+            }
+        }
+    },
+    totalAmount: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        validate: {
+            min: {
+                args: [0],
+                msg: "Total must be a positive number"
+            }
+        }
+    }
+});
+
+module.exports = Order;
--- db/models/index.js
+++ db/models/index.js
@@ -3,7 +3,9 @@ const Category = require("./Category");
 const Cart = require("./Cart");
 const LineItem = require("./LineItem");
 const User = require("./User");
+const Order = require("./Order");
 
+//associations
 Product.belongsTo(Category);
 Category.hasMany(Product);
 
@@ -11,5 +13,12 @@ Product.belongsToMany(Cart, { through: LineItem });
 
 Cart.belongsTo(User);
 User.hasOne(Cart);
+
+//hooks
+User.addHook("afterCreate", user =>
+  Cart.create({ status: "pending", userId: user.id })
+);
+
+
 
 module.exports = { Product, Category, Cart, LineItem, User };
