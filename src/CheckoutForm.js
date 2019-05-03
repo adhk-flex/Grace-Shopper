9744d4a4567df5a6ab040151005f999162b6c0be
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-03 18:12:12 -0400

    update checkout form

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -2,45 +2,98 @@
 import React, {Component} from 'react';
 
 class CheckoutForm extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            fullName: '',
+            email: '',
+            address: '',
+            zipCode: '',
+            sameShippAddress: true,
+            cardName: '',
+            cardNum: '',
+            expMonth: '',
+            expYear: '',
+            cvv: '',
+            errors: []
+        }
+    }
+
+    // componentDidUpdate(prevProps){
+
+    // }
+
+    onSave = (ev) => {
+        ev.preventDefault()
+    }
+
+    onChange = (ev) => {
+        // this.setState({[ev.target.name]:ev.target.value})
+        const target = ev.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        })
+    }
+
     render(){
+        const {onSave, onChange} =  this
+        const {fullName, email, address, zipCode, sameShippAddress, cardName, cardNum, expMonth, expYear, cvv} = this.state
+        const form = (addressType) => (
+            <form onSubmit={onSave}>
+                    <label htmlFor="fullName">Full Name</label>
+                    <input type="text" name="fullName" value={fullName} placeholder="John M. Eric" onChange = {onChange}/>
+                    <br/>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" value={email} placeholder="john@example.com" onChange = {onChange}/>
+                    <br/>
+                    <label htmlFor="address">Address</label>
+                    <input type="text" name="address" value = {address} placeholder="120 W 45th NYC" onChange = {onChange}/>
+                    <br/>
+                    <label htmlFor="zipCode">Zip Code</label>
+                    <input type="text" name="zipCode" value = {zipCode} placeholder="21003" onChange = {onChange}/>
+                    <br/>
+                    <button type='submit'>{`Save ${addressType}`}</button>
+            </form>
+        )
         return(
             <div>
                 <h3>Billing Address</h3>
-                <form>
-                    <label htmlFor="fullName">Full Name</label>
-                    <input type="text" name="fullName" placeholder="John M. Eric"/>
-                    <br/>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email" placeholder="john@example.com"/>
-                    <br/>
-                    <label htmlFor="address">Address</label>
-                    <input type="text" name="address" placeholder="120 W 45th NYC"/>
-                    <br/>
-                    <label htmlFor="zipCode">Zip Code</label>
-                    <input type="text" name="zipCode" placeholder="21003"/>
-                </form>
+                    {form('Billing Address')}
+                <label>Is the shipping address same as billing address?</label>
+                <input name='sameShippAddress' type='checkbox' checked={sameShippAddress} onChange={onChange}/>
+                {
+                    !sameShippAddress ? 
+                    <div>
+                        <h3>Shipping Address</h3>
+                        {form('Shipping Address')}
+                    </div> : null
+                }
                 <h3>Payment</h3>
                 <span>Accepted Cards</span>
                 <br/>
                 <span> Visa, Master, Amex, Discover</span>
-                <form>
-                    <label htmlFor="nameCard">Name on the Card</label>
-                    <input type="text" name="nameCard" placeholder="John Eric"/>
+                <form onSubmit={onSave}>
+                    <label htmlFor="cardName">Name on the Card</label>
+                    <input type="text" name="cardName" placeholder="John Eric" value={cardName} onChange = {onChange}/>
                     <br/>
                     <label htmlFor="cardNum">Credit Card Number</label>
-                    <input type="text" name="cardNum" placeholder="1111-2222-3333-4444"/>
+                    <input type="text" name="cardNum" placeholder="1111-2222-3333-4444" value={cardNum} onChange = {onChange}/>
                     <br/>
                     <label htmlFor="expMonth">Exp Month</label>
-                    <input type="text" name="expMonth" placeholder="August"/>
+                    <input type="text" name="expMonth" placeholder="August" value={expMonth} onChange = {onChange}/>
                     <br/>
                     <label htmlFor="expYear">Exp Year</label>
-                    <input type="text" name="expYear" placeholder="2020"/>
+                    <input type="text" name="expYear" placeholder="2020" value={expYear} onChange = {onChange}/>
                     <br/>
                     <label htmlFor="cvv">CVV</label>
-                    <input type="text" name="cvv" placeholder="345"/>
+                    <input type="text" name="cvv" placeholder="345" value={cvv} onChange = {onChange}/>
                     <br/>
+                    <button type='submit'>Save</button>
                 </form>
-                <button onClick={()=>{console.log('proceed to checkout')}}>Proceed To Checkout</button>
+                <button onClick={()=>{console.log('proceed to checkout page')}}>Proceed To Checkout</button>
             </div>
             
         )
