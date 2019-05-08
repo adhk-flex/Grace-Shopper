1d27c5363acc74dbccf885c1a33ab5dea5ef01c4
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-07 21:04:10 -0400

    fix bug on product list refresh

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,34 +1,58 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { lineItems, fetchLineItems } from './store/lineitem';
 
-const ProductList = (props) => {
-    const history = props.history;
-    const Products = props.products;
-    return(
-        <div>
-            <h1>Here are All of our Products:</h1>
-            <ul className='list-group'>
-                {
-                    Products.map(p=>{
-                        return (
-                            <li key={p.id} className='list-group-item'>
-                                <span>{p.name}</span>
-                                <img className='product-image' onClick={()=>history.push(`/product/${p.id}`)} src={p.imageUrl}/>
-                                <p>${p.price}</p>
-                            </li>)
-                    })
-                }
-            </ul>
-            <img className = 'shopping-cart' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKorRm0enmL_tFIgvKcNcOjb_3YkWnny-CIK0BW5F9DoGocc7DkA' onClick={()=>{history.push('/cart')}}/>
-        </div>
-    )
+class ProductList extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        }
     }
+    componentDidUpdate(prevProps){
+        if(prevProps.cart.id !== this.props.cart.id){
+            this.props.fetchLineItems(this.props.cart.id)
+        }     
+    }
+    render(){
+        const history = this.props.history;
+        const Products = this.props.products;
+        const totalItems = this.props.lineItems.reduce((acc, item) => {
+            acc += item.quantity
+            return acc
+        }, 0)
+        return(
+            <div>
+                <h1>Here are All of our Products:</h1>
+                <ul className='list-group'>
+                    {
+                        Products.map(p=>{
+                            return (
+                                <li key={p.id} className='list-group-item'>
+                                    <span>{p.name}</span>
+                                    <img className='product-image' onClick={()=>history.push(`/product/${p.id}`)} src={p.imageUrl}/>
+                                    <p>${p.price}</p>
+                                </li>)
+                        })
+                    }
+                </ul>
+                <img className = 'shopping-cart' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKorRm0enmL_tFIgvKcNcOjb_3YkWnny-CIK0BW5F9DoGocc7DkA' onClick={()=>{history.push('/cart')}}/>
+                <span className = 'shopping-item-quantity'>{totalItems}</span>
+            </div>
+        )
+    }
+}  
 
-
-const mapStateToProps = ({products}) => {
+const mapStateToProps = (state) => {
     return {
-        products: products
+        products: state.products,
+        lineItems: state.lineItems,
+        cart: state.cart
     }
 };
 
-export default connect(mapStateToProps)(ProductList);
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchLineItems: cartId => dispatch(fetchLineItems(cartId)),
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
