2b8e0b0ea40b0545e73348fc9dbd78076098cc10
Kristy Zhang <kristyso17@gmail.com>
2019-05-11 13:29:42 -0400

    add express review

---
+++ server/routes/review.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const db = require('../../db/models');
+const Review = db.Review;
+
+const router = express.Router();
+
+router.get('/:userId', (req, res, next) => {
+  Review.findAll({where: {userId: req.params.userId}})
+    .then((reviews) => res.json(reviews))
+    .catch(next);
+});
+
+router.get('/:productId', (req, res, next) => {
+  Review.findAll({where: {productId: req.params.productId}})
+    .then((reviews) => res.json(reviews))
+    .catch(next);
+});
+
+router.post('/:productId/:userId', (req, res, next) => {
+  let argument = {
+    content: req.body.content,
+    starts: req.body.start,
+    userId: req.params.userId,
+    productId: req.params.productId
+  }
+  Review.create(argument)
+    .then((reviews) => res.json(reviews))
+    .catch(next);
+})
+
+module.exports = router;
