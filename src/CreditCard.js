a2269f032214aae34387b643324e3fd418daec58
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-10 21:22:19 -0400

    working on credit card step2

--- src/CreditCard.js
+++ src/CreditCard.js
@@ -1,22 +1,24 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { creditCard } from './store/creditcards';
+import { getCreditCard, postCreditCard } from './store/creditcards';
 
 class CreditCard extends Component{
     constructor(){
         super()
-        this.state = {}
+        this.state = this.setCreditCard()
     }
 
-    componentDidUpdate(){
-
+    componentDidUpdate(prevProps){
+        if(prevProps.user.id !== this.props.user.id){
+            this.props.getCreditCard(this.props.user.id)
+        }
     }
 
     setCreditCard = (creditCard) => {
         return {
             firstName: creditCard ? creditCard.firstName : '',
             lastName: creditCard ? creditCard.lastName : '',
-            cardType: creditCard ? creditCard.type : '',
+            cardType: creditCard ? creditCard.type : 'visa',
             cardNumber: creditCard ? creditCard.cardNumber : '',
             cvv: creditCard ? creditCard.cvv : '',
             expMonth: creditCard ? creditCard.expMonth : '',
@@ -24,13 +26,61 @@ class CreditCard extends Component{
         }
     }
 
+    onChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    onSave = (e) => {
+        e.preventDefault()
+        this.props.postCreditCard(this.props.user.id, {...this.state, number: this.state.cardNumber})
+    }
+
     render(){
+        const {firstName, lastName, cardNumber, cardType, expMonth, expYear, cvv} = this.state
+        const {onChange, onSave} = this
         return (
             <div>
                 <h3>Credit Card Information</h3>
+                <form onSubmit={onSave}>
+                    <label htmlFor='firstName'>firstName</label>
+                    <input text='firstName' name='firstName' value={firstName} onChange={onChange}/>
+                    <br/>
+                    <label htmlFor='lastName'>lastName</label>
+                    <input text='lastName' name='lastName' value={lastName} onChange={onChange}/>
+                    <br/>
+                    <label htmlFor='cardType'>cardType</label>
+                    <input text='cardType' name='cardType' value={cardType} onChange={onChange}/>
+                    <br/>
+                    <label htmlFor='cardNumber'>cardNumber</label>
+                    <input text='cardNumber' name='cardNumber' value={cardNumber} onChange={onChange}/>
+                    <br/>
+                    <label htmlFor='expMonth'>expMonth</label>
+                    <input text='expMonth' name='expMonth' value={expMonth} onChange={onChange}/>
+                    <br/>
+                    <label htmlFor='expYear'>expYear</label>
+                    <input text='expYear' name='expYear' value={expYear} onChange={onChange}/>
+                    <br/>
+                    <label htmlFor='cvv'>cvv</label>
+                    <input text='cvv' name='cvv' value={cvv} onChange={onChange}/>
+                    <br/>
+                    <button type ='submit'>Save Credit Card</button>
+                </form>
             </div>
         )
     }
 }
 
-export default CreditCard;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getCreditCard: (userId) => dispatch(getCreditCard(userId)),
+        postCreditCard: (userId, cardInfo) => dispatch(postCreditCard(userId, cardInfo))
+    }
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(CreditCard);
