b54cca56ac0acf5e7802ffb530efc8ca818c5bda
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-11 12:32:06 -0400

    adding errors handling in ShipAddress

--- src/ShipAddress.js
+++ src/ShipAddress.js
@@ -2,20 +2,35 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import {userAddress, postAddress} from './store/address';
+import Errors from './Errors';
 
 class ShipAddress extends Component{
 
     constructor(props){
         super(props)
-        this.state = this.userShipAddress()
+        this.state = {...this.userShipAddress(), errors:[]}
+    }
+
+    componentDidMount(){
+        this._mounted = true
     }
 
     componentDidUpdate(prevProps){
-        if(JSON.stringify(prevProps)!==JSON.stringify(this.props)){
+        if(JSON.stringify(prevProps.address)!==JSON.stringify(this.props.address)){
             this.props.getShipAddress(this.props.user.id, 'shipping')
-            .then(({address})=>this.setState({...address}))
+            .then(({address})=>{
+                if(this._mounted){
+                    this.setState({...address})
+                }
+            })
         }
     }
+
+    componentWillUnmount(){
+        this._mounted = false  
+    }
+
+    _mounted = false
 
     userShipAddress = (address) => {
         return {
@@ -32,10 +47,17 @@ class ShipAddress extends Component{
     onChange = (e) => {
         this.setState({[e.target.name]: e.target.value})
     }
+
+    onSave = (e) => {
+        e.preventDefault()
+        this.props.postAddress(this.state, this.props.user.id, 'shipping')
+        .then(()=>{this.props.history.push('/checkoutStep2')})
+        .catch((e)=>{this.setState({errors: e.response.data.errors})})
+    }
     
-    Addressform = (firstName, lastName, addressLine1, addressLine2, zip, state, city, onChange) => (
+    Addressform = (firstName, lastName, addressLine1, addressLine2, zip, state, city, onChange, onSave) => (
         <div>
-        <form>
+            <form onSubmit={onSave}>
                 <label htmlFor={`firstName`}>First Name</label>
                 <input type="text" name={`firstName`} value={firstName} onChange = {onChange}/>
                 <br/>
@@ -56,26 +78,21 @@ class ShipAddress extends Component{
                 <br/>
                 <label htmlFor={`city`}>City</label>
                 <input type="text" name={`city`} value = {city} onChange = {onChange}/>
-        </form>
-        <button  
-            onClick={
-                ()=>{
-                    this.props.postAddress(this.state, this.props.user.id, 'shipping')
-                    this.props.history.push('/checkoutStep2')
-                }
-            }
-        >save and proceed</button>
+                <br/>
+                <button type='submit'>Save and Proceed</button>
+            </form>
+            <Errors errors={this.state.errors}/>
         </div>
     )
 
     render(){
         const {firstName, lastName, addressLine1, addressLine2, zip, state, city} = this.state
-        const {onChange} = this
+        const {onChange, onSave} = this
         return(
             <div>
                 <h3>Shipping Address</h3>
                 {
-                    this.Addressform(firstName, lastName, addressLine1, addressLine2, zip, state, city, onChange)
+                    this.Addressform(firstName, lastName, addressLine1, addressLine2, zip, state, city, onChange, onSave)
                 }
             </div>
         )
