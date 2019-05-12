398a815b113503d5785fe8e497b1428c1eedc5c8
Kristy Zhang <kristyso17@gmail.com>
2019-05-12 09:50:50 -0400

    edit/delete in cart localStorage

--- src/store/lineitem.js
+++ src/store/lineitem.js
@@ -45,31 +45,34 @@ export const addLineItem = (product, cartId) => dispatch => {
 };
 
 export const delLineItem = (item) => dispatch => {
-  if(item.cartId === undefined){
+  if (item.cartId === undefined){
     let items = JSON.parse(localStorage.getItem('lineItems'))  
-    localStorage.setItem('lineItems', JSON.stringify(items.filter(item=>item.productId!==item.productId)))
+    localStorage.setItem('lineItems', JSON.stringify(items.filter(i => i.productId !== item.productId)))
     return new Promise(() => dispatch(fetchLineItems()))
   }
-  else{
+  else {
     return axios.delete(`/api/lineitems/${item.id}`)
     .then(() => dispatch(fetchLineItems(item.cartId)))
   }
 };
 
-export const updateLineItem = (id, formData, cartId) => dispatch => {
-  if(cartId===undefined){
-    console.log(formData)
-    let items = JSON.parse(localStorage.getItem('lineItems'))  
-    let newitems = items.filter(item=>item.productId!==formData.productId)
-    if(!newitems){
-      newitems=[]
+export const updateLineItem = (item) => dispatch => {
+  if (item.cartId === undefined) {
+    let items = JSON.parse(localStorage.getItem('lineItems'))
+    // console.log('items in store', items)
+    let newitems = items.filter(i => i.productId !== item.productId)
+    // console.log('newitems in store', newitems)
+    // console.log(item)
+    if (!newitems) {
+      newitems = []
     }
-    newitems.push(formData)
+    newitems.push(item)
     localStorage.setItem('lineItems', JSON.stringify(newitems))
     return new Promise(() => dispatch(fetchLineItems()))
   }
-  else{
-    return axios.put(`/api/lineitems/${id}`, formData)
-    .then(() => dispatch(fetchLineItems(cartId)))
+  else {
+    const {id, cartId} = item
+    return axios.put(`/api/lineitems/${id}`, item)
+      .then(() => dispatch(fetchLineItems(cartId)))
   }
 }
