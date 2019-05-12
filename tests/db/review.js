ba26d1e034e43a4b55b405e9259e3be37c1ba94c
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-05-12 16:13:40 -0400

    add unit test for user model

--- tests/db/review.js
+++ tests/db/review.js
@@ -1,5 +1,6 @@
 const expect = require("chai").expect;
 const { Review, Product, User } = require("../../db");
+const db = require("../../db")
 
 const testReview1 = {
     content: "This product is awesome!",
@@ -12,40 +13,43 @@ const testReview2 = {
 }
 
 describe("review model", ()=>{
-    it("can create a review with correct content and star given an review object", done=>{
-        Review.create(testReview1)
-            .then(review => {
-                expect(review.content).to.equal("This product is awesome!")
-                expect(review.stars).to.equal(5)
-            })
-            .then(()=>done())
-            .catch(e=>done(e))
+    describe("review model", () => {
+        it("can create a review with correct content and star given an review object", done=>{
+            Review.create(testReview1)
+                .then(review => {
+                    expect(review.content).to.equal("This product is awesome!")
+                    expect(review.stars).to.equal(5)
+                })
+                .then(()=>done())
+                .catch(e=>done(e))
+        })
+        it("can create a review for a product", done=>{
+            Product.findOne()
+                .then(product=>{
+                    Review.create({...testReview2, productId: product.id})
+                        .then(review => {
+                            expect(review.productId).to.equal(product.id)
+                        })
+                })
+                .then(() => done())
+                .catch(e => done(e))
+        })
+        it("can create a reivew from a user for a given product", done=>{
+            User.findOne()
+                .then(user=>{
+                    Product.findOne()
+                        .then(product=>{
+                            Review.create({...testReview2, productId: product.id, userId: user.id})
+                                .then(review => {
+                                    expect(review.productId).to.equal(product.id)
+                                    expect(review.userId).to.equal(user.id)
+                                })
+                        })
+                })
+                .then(()=> done())
+                .catch(e=> done(e))
+        })
     })
-    it("can create a review for a product", done=>{
-        Product.findOne()
-            .then(product=>{
-                Review.create({...testReview2, productId: product.id})
-                    .then(review => {
-                        expect(review.productId).to.equal(product.id)
-                    })
-            })
-            .then(() => done())
-            .catch(e => done(e))
-    })
-    it("can create a reivew from a user for a given product", done=>{
-        User.findOne()
-            .then(user=>{
-                Product.findOne()
-                    .then(product=>{
-                        Review.create({...testReview2, productId: product.id, userId: user.id})
-                            .then(review => {
-                                expect(review.productId).to.equal(product.id)
-                                expect(review.userId).to.equal(user.id)
-                            })
-                    })
-            })
-            .then(()=> done())
-            .catch(e=> done(e))
-    })
+    
 })
 
